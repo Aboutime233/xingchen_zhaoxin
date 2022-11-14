1916dba66c2053407fbe794022ecd6e167878de8
Aboutime233 <1139827051@qq.com>
2022-11-14 09:54:06 +0800

    last one

---
+++ html5/request-demo/src/main/java/com/sjh/web/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.sjh.web;
+
+import com.sjh.mapper.UserMapper;
+import com.sjh.pojo.User;
+import com.sjh.util.SqlSessionFactoryUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtils.getSqlSessionFactory();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        User user = userMapper.select(username, password);
+        sqlSession.close();
+
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        if(user != null){
+            // 登陆成功
+            writer.write("登陆成功");
+        }else {
+            // 登陆失败
+            writer.write("登陆失败");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
